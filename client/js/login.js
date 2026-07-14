37a14fcad5c0ee95099b13ab96ed6377e6fbe774
prem kumar <mpremkumar426@gmail.com>
2026-07-14 22:42:43 +0530

    Updated API URLs for Render deployment

--- client/js/login.js
+++ client/js/login.js
@@ -99,7 +99,7 @@
 
       try {
 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://coffee-shop-uiom.onrender.com/api/auth", {
 
           method: "POST",
 
@@ -224,7 +224,7 @@
 
       try {
 
-        const response = await fetch("http://localhost:5000/api/auth/signup", {
+        const response = await fetch("https://coffee-shop-uiom.onrender.com/api/auth", {
 
           method: "POST",
 
--- client/js/signup.js
+++ client/js/signup.js
@@ -29,7 +29,7 @@ if (signupForm) {
 
         try {
 
-            const response = await fetch("http://localhost:5000/api/auth/signup", {
+            const response = await fetch("https://coffee-shop-uiom.onrender.com/api/auth", {
 
                 method: "POST",
 
