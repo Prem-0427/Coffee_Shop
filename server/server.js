54a5e151a724fc2cc1607f73b558a6686abdb892
prem kumar <mpremkumar426@gmail.com>
2026-07-14 21:12:56 +0530

    Updated API URLs for Render deployment

--- server/server.js
+++ server/server.js
@@ -19,7 +19,9 @@ const app = express();
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://coffee-shop-client.netlify.app",
+        "https://coffee-shop-admin.netlify.app"
     ],
     credentials: true
 }));
